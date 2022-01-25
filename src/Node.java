ecccd5018edab55c91910f8a1724e908ad82c966
Ravi Raghavan <ravi0308raghavan@gmail.com>
2022-01-25 18:31:43 -0500

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,25 @@
+/**
+ * Returns a Node Object for use in Song Library
+ * @author Ravi Raghavan
+ */
+public class Node {
+
+    /**
+     * Does something
+     * @param x the name of the argument
+     * @param y the name of the argument
+     */
+    public Node (int x, int y){
+
+    }
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    public String printName(String name){
+        return "Ravi Raghavan";
+    }
+
+}
